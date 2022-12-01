29d3cc13c8781d23ea1ab203671492123857f037
VillacisDavid <david.villacis@unmsm.edu.pe>
2022-12-01 17:58:42 -0500

    Feat: Routing Agregado
    Tambien algunos cambios menores

--- 02 DESARROLLO/DSEVI/02 Sprint 2/01 Desarrollo/frontend/src/app/app-routing.module.ts
+++ 02 DESARROLLO/DSEVI/02 Sprint 2/01 Desarrollo/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaginaDashboardComponent } from './componentes/pagina-dashboard/pagina-dashboard.component';
+import { PaginaInicioComponent } from './componentes/pagina-inicio/pagina-inicio.component';
+import { PaginaLoginComponent } from './componentes/pagina-login/pagina-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: 'inicio', component: PaginaInicioComponent },
+  { path: 'dashboard', component: PaginaDashboardComponent },
+  { path: 'login', component: PaginaLoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
